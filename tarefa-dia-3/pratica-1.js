d3dbee7be1bc135520f7bb846c35c7562d2b3dda
Marcelo Araújo <Marcelo P Araujo>
2022-06-24 17:25:27 -0300

    Criação de variáveis no arquivo pratica-1.js

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,8 @@
+// declarando variáveis
+let nome = "marcelo"
+let email = "marceloaraujo.pro@gmail.com"
+let profissao = "analista dev full stack"
+let idade = 47
+let lugar = "Canadá"
+
+console.log(nome)
