4d584a23bbe44b7b623762edbf7a34f4b6e6d072
Marcelo Araújo <Marcelo P Araujo>
2022-06-24 17:40:10 -0300

    mostrado os valores na tela com o comando console.log()

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -5,4 +5,8 @@ let profissao = "analista dev full stack"
 let idade = 47
 let lugar = "Canadá"
 
-console.log(nome)
+console.log("Nome:",nome)
+console.log("E-mail:",email)
+console.log("Profissão:",profissao)
+console.log("Idade:",idade)
+console.log("Lugar que gostaria de conhecer:",lugar)
